f4c7cbce4cd05ecd58172cae577079d280728dc2
Juan David Monroy <jdmonroyr@gmail.com>
2016-09-15 16:57:19 -0500

    fixed header pop-up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module("woldApp", ["ui.router", "supplier.controllers"])
+angular.module("woldApp", ["ui.router", "ngAnimate", "ui.bootstrap", "supplier.controllers"])
 
 .config(function ($stateProvider, $urlRouterProvider) {
-    
+
 $stateProvider
 
 .state("suppliers", {
@@ -11,9 +11,9 @@ $stateProvider
     controller : "supplierController",
     templateUrl : "views/supplierList.html"
 })
-   
-    
+
+
 $urlRouterProvider.otherwise("/suppliers");
-    
-    
+
+
 });
